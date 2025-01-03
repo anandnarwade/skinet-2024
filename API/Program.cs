b345f0f186045ca3d03ff109788956f133617797
Anand Narwade <anand.narwade11@gmail.com>
2025-01-03 22:29:06 +0530

    End of Section 3: Implement product repository and seed data for delivery options

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,12 +13,26 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 
 app.MapControllers();
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    // var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    // logger.LogError(ex, "An error occurred during migration");
+    Console.WriteLine(ex);
+}
 
 app.Run();
